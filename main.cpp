826b1201b3fd6dd4e5dc353d702520815aaf1b6b
KHAAdotPK <spysee.pk@gmail.com>
2026-01-04 00:02:08 +0500

    Changed the name of keys::generate_keys() to keys::generate_key(. We are generating just one key per call, not many. I forgot to update main.cpp, it was still using the old name.

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ int main()
     
     cc_tokenizer::String<char> str("hello");
     
-    std::cout << keys::generate_keys(str, 10) << std::endl;
+    std::cout << keys::generate_key(str, 10) << std::endl;
 
-    std::cout << keys::generate_keys("start", 10) << std::endl;
+    std::cout << keys::generate_key("start", 10) << std::endl;
    
     return 0;
 }
